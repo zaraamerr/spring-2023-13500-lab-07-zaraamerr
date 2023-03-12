7a7558c734bbd61d03a19c8d30fb7f7ced01ab4c
zaraamerr <zaraa.amerr@gmail.com>
2023-03-11 20:01:34 -0500

    added bad code that i can test on

---
+++ bad-code.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+//file to store bad code!!
+
+int main(){
+           // Hi, I'm a program!
+int x = 1; 
+    for(int i = 0; i < 10; i++) {
+std::cout << i;
+          std::cout << std::endl;
+ }
+    }
